48c0b07046b4b594ed545c14b83a18346969340c
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2017-10-17 08:51:57 +0300

    Create webasto00.ino

---
+++ test/webasto00.ino
@@ -0,0 +1,79 @@
+// sistem control Webasto
+// program scris de Nicu FLORICA (niq_ro)
+// program testat de George MORSOACA (Tommy)
+// versiune 0, simulare program simplu - 16.10.2017, Craiova
+
+#define pompita 10
+#define ventilator 11
+#define bujie 9
+int aerisire = 10000;  // timp pentru ventilare camera (ms)
+int tpbujie = 5; // timp mentinere pornita bujie (s)
+int tppompa1 = 500;  // timp alimentare pompa la aprindere
+int tppompa2 = 1000; // timp alimentare pompa la functionare normala
+
+void setup() {
+Serial.begin(9600);
+Serial.println("Simulare control Webasto ...");
+  
+pinMode(pompita, OUTPUT);
+pinMode(ventilator, OUTPUT);
+pinMode(bujie, OUTPUT);
+
+digitalWrite(pompita, 0);  // pompita oprita
+analogWrite(ventilator, 0); // ventilator oprit
+digitalWrite(bujie, 0); // bujie stinsa
+
+// se simuleaza asteptarea
+Serial.println("Sistem in asteptare !");
+Serial.println("---------------------------------");
+delay(3000);
+
+// simulam apasare buton de pornire sau comanda de pornire
+Serial.println("Se da comanda de pornire !");
+// 1) aerisire camera de gaze
+analogWrite(ventilator, 255); // ventilator pornit la turatie maxima
+Serial.println("Porneste ventilator pentru aerisire camera !");
+delay(aerisire);   
+analogWrite(ventilator, 0); // ventilator oprit
+Serial.println("Se opreste ventilatorul !");
+delay(1000); // mica pauza
+
+Serial.println("---------------------------------");
+Serial.println("Se initializeaza procedura de aprindere !");
+// 2) alimentare bujie, pompa alimentata cu 2 impulsuri pe minut, turatie ventilator in crestere
+digitalWrite(bujie, 1);  // alimentare bujie
+Serial.println("Bujie aprinsa !");
+delay(200); // mica intarziere
+
+for (int i = 1; i<=tpbujie; i++)
+{
+  analogWrite(ventilator, 255*i/tpbujie);  // crestere turatie pompita
+  Serial.print("Turatie ventilator: ");
+  Serial.print(255*i/tpbujie);
+  Serial.println("/255");
+  digitalWrite(pompita, 1); // alimentare pompita
+  Serial.println("Pompita alimentata !");
+  delay(tppompa1);
+  digitalWrite(pompita, 0); // oprire pompita
+  Serial.println("Pompita oprita !");
+  delay(tppompa1);
+}
+digitalWrite(bujie, 0);  // decuplare bujie
+Serial.println("Bujie stinsa !");
+Serial.println("Terminare secventa de aprindere !");
+Serial.println("---------------------------------");
+analogWrite(ventilator, 255);  // ventilator la turatie maxima
+  Serial.print("Turatie ventilator: ");
+  Serial.print("255");
+  Serial.println("/255");
+}
+
+void loop() {
+// 3) functionare normala: ventilator la turatie maxima si pompita alimentata la 1 secunda
+  digitalWrite(pompita, 1); // alimentare pompita
+  Serial.println("Pompita alimentata !");
+  delay(tppompa2);
+  digitalWrite(pompita, 0); // oprire pompita
+  Serial.println("Pompita oprita !");
+  delay(tppompa2);
+}
